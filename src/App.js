ce0f255488bf1001ba7f59f8bc8631f086fad070
Trisha Lim <limtrishajean@gmail.com>
2020-09-19 09:35:23 +0700

    change app content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import './App.css';
 import Example from './submodule/Example';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to my app.
-      </header>
       <div>
-        <h3>Here's a component reused from the library</h3>
         <Example text="Example component used from App.js" />
       </div>
     </div>
