963f1b026caac6bdcea14383022d7756ad50a926
Trisha Lim <limtrishajean@gmail.com>
2020-09-20 12:43:05 +0700

    use components from library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Button } from 'react-rollup-boilerplate';
+import React, { useState } from 'react';
+import { Button, Card, SideNav } from 'react-rollup-boilerplate';
+
+import './App.css';
 
 function App() {
+
+  const [activePage, setActivePage] = useState('Home');
+
+  const items = [
+    { name: 'Home', onClick: () => setActivePage('Home') },
+    { name: 'Account', onClick: () => setActivePage('Account') },
+    { name: 'Settings', onClick: () => setActivePage('Settings') },
+    { name: 'Help', onClick: () => setActivePage('Help') }
+  ];
+
   return (
     <div className="App">
-      <div>
-        <Button text="world" />
+      <SideNav items={items} />
+      <div className="App__content">
+        <Card>
+          <h1>{activePage}</h1>
+          <Button
+            text="Back to Home"
+            onClick={() => setActivePage('Home')}
+          />
+        </Card>
       </div>
     </div>
   );
